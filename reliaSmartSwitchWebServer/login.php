79b2dbe16f81a423c8c83b2254de1342a83b7eb4
thaschbe <thomas.haschberger@aon.at>
2016-12-28 16:54:00 +0100

    login and register page and some css updates

--- reliaSmartSwitchWebServer/login.php
+++ reliaSmartSwitchWebServer/login.php
@@ -4,13 +4,14 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>reliaSmartSwitch</title>
+        <title>reliaSmartSwitch Login</title>
         <link rel="stylesheet" href="styles/main.css">
     </head>
 
-    <h1> reliaSmartSwitch Home</h1>
+    <h1> reliaSmartSwitch Login</h1>
     <a href="login.php">login</a>
     <a href="register.php">register</a>
+    <a href="smartswitchuser.php">user</a>
     <?php
        echo "Webpage under construction";
        echo "(c) Thomas Haschberger 2016";
---
+++ reliaSmartSwitchWebServer/register.php
@@ -0,0 +1,41 @@
+﻿<!DOCTYPE html>
+<!--reliaSmartSwitch 2016 -->
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>reliaSmartSwitch</title>
+        <link rel="stylesheet" href="styles/style.scss">
+    </head>
+
+    <body>
+    <h1> reliaSmartSwitch Register</h1>
+    <a href="login.php">login</a>
+    <a href="register.php">register</a>
+
+    <div id="wrap">
+    <div id="regbar">
+      <div id="navthing">
+        <h2><a href="#" id="loginform">Login</a> | <a href="#">Register</a></h2>
+      <div class="login">
+        <div class="arrow-up"></div>
+        <div class="formholder">
+          <div class="randompad">
+             <fieldset>
+               <label name="email">Email</label>
+               <input type="email" value="example@example.com" />
+               <label name="password">Password</label>
+               <input type="password" />
+               <input type="submit" value="Login" />
+
+             </fieldset>
+          </div>
+        </div>
+        </div>
+        </div>
+    </div>
+    </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/login_example.js"></script>
+    </body>
+</html>
